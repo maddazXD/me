33dae6a066298547cd807cd8de8fc959b8c7621b
maddazXD <maddazryu14@gmail.com>
2026-08-28 18:38:04 +0700

    Delete temp directory

--- temp/fetch_yt.cjs
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-https.get('https://html.duckduckgo.com/html/?q=site:youtube.com+dj+indonesia', {headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}}, (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => {
-    const matches = data.match(/v=([a-zA-Z0-9_-]{11})/g) || [];
-    console.log([...new Set(matches)]);
-  });
-});
